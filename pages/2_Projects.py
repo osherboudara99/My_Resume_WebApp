6927495b55789544e82545cf5107f80c03afecf0
osherboudara99 <osherboudara99@gmail.com>
2025-05-27 15:59:10 -0400

    removed projects page

--- pages/2_Projects.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-import utils.backend_utils as backend
-import utils.frontend_utils as frontend
-import utils.chatbot_utils as chatbot
-
-st.set_page_config(layout="wide", page_icon=':office_worker:')
-frontend.load_css()
-chatbot.create_sidebar()
