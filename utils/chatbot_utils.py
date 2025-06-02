6966e2ea50cc453e082a47169ebd56c7f5611704
osherboudara99 <osherboudara99@gmail.com>
2025-06-02 10:33:22 -0700

    added pysqlite code back

--- utils/chatbot_utils.py
+++ utils/chatbot_utils.py
@@ -3,10 +3,10 @@ import openai
 from sentence_transformers import SentenceTransformer
 import sys
 # Add pysqlite3 support for Streamlit Cloud
-# import platform
-# if platform.system() != "Windows":
-#     __import__('pysqlite3')
-#     sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+import platform
+if platform.system() != "Windows":
+    __import__('pysqlite3')
+    sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 
 
 import chromadb
