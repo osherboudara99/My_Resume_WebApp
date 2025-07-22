4b0b01576f533065978b1d48f729b984eebfdbb7
osherboudara99 <osherboudara99@gmail.com>
2025-07-22 11:33:31 -0700

    edit

--- utils/frontend_utils.py
+++ utils/frontend_utils.py
@@ -28,7 +28,7 @@ def certification_view(cert_name, cert_name_display, credential_link=None, dir_p
 
     with st.container():
         with st.expander(f'View {cert_name_display} PDF'):
-            pdf_viewer(cert_path, width="100%", height=1000)
+            pdf_viewer(cert_path, width=700, height=1000)
             if credential_link:
                 st.markdown(f'Link to [{cert_name_display}]({credential_link}) credential/badge')
         
