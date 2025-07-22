29808091b9f0746d151e223dd1a0a419fd167906
osherboudara99 <osherboudara99@gmail.com>
2025-07-22 11:53:30 -0700

    edit

--- utils/frontend_utils.py
+++ utils/frontend_utils.py
@@ -6,29 +6,7 @@ import utils.backend_utils as backend
 import time
 
 
-st.markdown("""
-    <style>
-    /* Make all radio option text white */
-    div[data-baseweb="radio"] > div > div {
-        color: white !important;
-    }
 
-    /* Also target the inner span text inside radio buttons */
-    div[data-baseweb="radio"] span {
-        color: white !important;
-    }
-
-    /* Optional: change hover/focus color */
-    div[data-baseweb="radio"] div:hover {
-        color: white !important;
-    }
-
-    /* Make radio label (e.g., "Order:") white */
-    div.stRadio label {
-        color: white !important;
-    }
-    </style>
-""", unsafe_allow_html=True)
 
 dir_path = os.getcwd()
 
